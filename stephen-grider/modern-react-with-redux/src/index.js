18550a97eeecaae79458034363d16f4dc5ff2b51
Kevin Maduro <nivekzin@gmail.com>
2019-01-31 23:13:55 -0200

    modern-react: Adding props, faker and created a component for comment details

--- stephen-grider/modern-react-with-redux/src/index.js
+++ stephen-grider/modern-react-with-redux/src/index.js
@@ -2,12 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import CommentDetail from './comment-detail';
+
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>
-}
+  return <div>
+    <CommentDetail
+      author="Sam"
+      timeAgo="Today at 4:00PM"
+    />
+    <CommentDetail
+      author="Alex"
+      timeAgo="Today at 2:00AM"
+    />
+    <CommentDetail
+      author="Jane"
+      timeAgo="Yesterday at 5:25PM"
+    />
+  </div>
+};
 // take the react component and show it in the screen
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
-)
+  document.querySelector('#root')
+);
