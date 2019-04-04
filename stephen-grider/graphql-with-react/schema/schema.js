ec1d2c092a87205914f2d41a196013174031d741
Kevin Maduro <kevin.silva@stormsec.com.br>
2019-04-04 08:59:28 -0300

    stephen-graphql: Added arrow fucntion to graphQL fields statement.

--- stephen-grider/graphql-with-react/schema/schema.js
+++ stephen-grider/graphql-with-react/schema/schema.js
@@ -6,21 +6,29 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
-    name: "Company",
-    fields: {
+    name: 'Company',
+    fields: () => ({
         id: { type: GraphQLInt },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:5000/companies/${parentValue.id}/users`)
+                    .then(resp => resp.data)
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
-        id: { type: GraphQLString },
+    fields: () => ({
+        id: { type: GraphQLInt },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
         company: {
@@ -30,7 +38,7 @@ const UserType = new GraphQLObjectType({
                     .then(resp => resp.data)
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -38,9 +46,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         user: {
             type: UserType,
-            args: { id: { type: GraphQLString }},
+            args: { id: { type: GraphQLInt }},
             resolve(parentValue, args) {
-                return axios.get(`http://localhost:5000/users/${args.id}`)
+                return axios.get(`http://localhost:5000/users/${parentValue.id}`)
+                    .then(resp => resp.data)
+            }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLInt }},
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:5000/companies/${args.id}`)
                     .then(resp => resp.data)
             }
         }
