38c580372339091f015a64b9f05d57b83977841b
Kevin Maduro <nivekzin@gmail.com>
2019-04-07 17:20:23 -0300

    elixir-beginners: Created first file with some basic concepts

---
+++ elixir-for-begginers/hello_world.exs
@@ -0,0 +1,26 @@
+# Hello World
+IO.puts("Hello World!")
+
+IO.puts String.downcase "SAUSAGE"
+
+IO.puts 1 + 2
+
+IO.puts 10/2
+
+IO.puts 1_000_000
+
+IO.puts 0o444
+
+IO.puts 3141.0e-3
+
+IO.puts 3141.0e3
+
+IO.puts div(10, 2)
+
+IO.puts div(9, 2)
+
+IO.puts rem(10, 3)
+
+name = "elixir"
+
+IO.puts "Hello #{name}"
