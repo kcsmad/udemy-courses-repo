ac7817dd3d1a3736c6100ae1679c97b1b6222a44
Kevin Maduro <kevin.silva@stormsec.com.br>
2019-04-11 22:15:40 -0300

    stephen-graphql: Updating data with graphql

--- stephen-grider/graphql-with-react/schema/schema.js
+++ stephen-grider/graphql-with-react/schema/schema.js
@@ -78,6 +78,29 @@ const mutation = new GraphQLObjectType({
                 return axios.post('http://localhost:5000/users', { firstName, age })
                   .then(res => res.data);
             }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:5000/users/${id}`)
+                    .then(res => res.data)
+            }
+        },
+        updateUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLInt) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:5000/users/${args.id}`, args)
+                    .then(res => res.data)
+            }
         }
     }
 });
